bbc07ad58d2bde1696b20f613d156234e1215deb
mehak16163 <mehak16163@iiitd.ac.in>
2018-03-16 13:45:03 +0530

    chat_bot is ready :D

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+all :
+	gcc -o chat_room chat_room.c
+
+clean:
+	rm chat_room
--- chat_room.c
+++ chat_room.c
@@ -4,22 +4,56 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-
+#include <errno.h>
+extern int errno;
 int main(){
-	printf("Select an option: \n1. Sign up\n2. Login\n");
+	printf("Select an option: \n1. Sign up\n2. Login\n3. Create a new group\n");
 	char user_name[1000];
 	scanf("%s", user_name);
 	FILE * fp;
+	FILE *fp1;
 	char * option = user_name;
 	char * current;
 	char  id[100];
 	if(strcmp(option,"1")==0)
 	{
 		printf("Please enter your username: \n");
-		scanf("%s",id);
-		//id = user_name;
+		fp1=fopen("groups.txt","a");
+		char name[100]="";
+		getchar();
+		scanf("%[^\n]",id);
+		strcat(name,id);
 		strcat(id,".txt");
-		fp = fopen(id,"w");
+		fp = fopen(id,"a");
+		fclose(fp);
+		char input[100]="";
+		strcat(input,name);
+		strcat(input," ");
+		strcat(input,name);
+		strcat(input," \n");
+		fprintf(fp1,"%s",input);
+		fclose(fp1);
+		return 0;
+	}
+	else if(strcmp(option,"3")==0){
+		fp = fopen("groups.txt","a");
+		printf("Enter number of members: \n");
+		int num;
+		scanf("%d",&num);
+		printf("Enter group name: \n");
+		char group[100];
+		getchar();
+		scanf("%[^\n]",group);
+		strcat(group," ");
+		fprintf(fp,"%s",group );
+		for(int j=0;j<num;j++){
+			printf("%d. Enter user id: \n",j+1);
+			char name[50];
+			scanf("%s",name);
+			strcat(name, " ");
+			fprintf(fp, "%s",name);
+		}
+		fprintf(fp," \n");
 		fclose(fp);
 		return 0;
 	}
@@ -27,7 +61,6 @@ int main(){
 	{
 		printf("Please enter your username: \n");
 		scanf("%s", id);
-		//id = user_name;
 	}
 	while(1){
 		printf("Select Option:\n1. Read\n2. Write\n3. Exit\n");
@@ -42,22 +75,56 @@ int main(){
 			char msg[1000];
 			getchar();
 			scanf("%[^\n]", msg);
-			printf("Enter number of recipients: \n");
+			printf("Enter recipient: \n");
+			fp = fopen("groups.txt","r");
 			scanf("%s", user_name);
-			int num = atoi(user_name);
-			for(int i=0;i<num;i++){
-				printf("Enter id of recipients\n");
-				scanf("%s", user_name);
-				strcat(user_name,".txt");
-				key_t key = ftok(user_name,65);
+			char* line=NULL;
+			size_t len=0;
+			int read=0;
+			char *token;
+			while((read=getline(&line,&len,fp))!=-1){
+				token = strtok(line," ");
+				if(strcmp(token,user_name)==0){
+					break;
+				}
+			}
+			fclose(fp);
+			if(read==-1){
+				printf("Invalid recipient\n");
+				continue;
+			}
+			token=strtok(NULL," ");
+			while(token!=NULL ){
+				if(strcmp(token,"\n")==0){
+					break;
+				}
+				char send[100]="";
+				strcat(send,token);
+				getchar();
+				strcat(send,".txt");
+				key_t key = ftok(send,65);
 				int shmid = shmget(key,1024,0666|IPC_CREAT);
+				if(shmid==-1){
+					int errnum = errno;
+					printf("Value of error %d\n",errnum);
+					perror("Unable to get id");
+					printf("%s\n",strerror(errnum));
+				}
 				char *str = (char *) shmat(shmid,(void *)0,0);
+				if((void *)str ==(void *)-1){
+					int errnum = errno;
+					printf("Value of error %d\n",errnum);
+					perror("Unable to attach to shared memory.");
+					printf("%s\n",strerror(errnum));
+				}
 				strcat(str,"\n");
 				strcat(str , id);
 				strcat(str,": ");
 				strcat(str, msg);
 				shmdt(str);
+				token=strtok(NULL," ");
 			}
+			
 		}
 		else
 		{
@@ -65,7 +132,19 @@ int main(){
 				strcat(id1,".txt");
 				key_t key = ftok(id1,65);
 				int shmid = shmget(key,1024,0666|IPC_CREAT);
+				if(shmid==-1){
+					int errnum = errno;
+					printf("Value of error %d\n",errnum);
+					perror("Unable to get id");
+					printf("%s\n",strerror(errnum));
+				}
 				char *str = (char*) shmat(shmid,(void*)0,0);
+				if((void *)str == (void *)-1){
+					int errnum = errno;
+					printf("Value of error %d\n",errnum);
+					perror("Unable to attach to shared memory.");
+					printf("%s\n",strerror(errnum));
+				}
 				printf("%s\n", str);
 				shmdt(str);
 				shmctl(shmid,IPC_RMID,NULL);
